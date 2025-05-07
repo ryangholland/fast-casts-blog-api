74176b460eda9118c4230f8d59d2ea167b155107
ryangholland <rghcode@gmail.com>
2025-05-07 18:22:20 -0400

    feat: wire login route into app and test with Postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,8 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Base test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+// Register /auth routes
+app.use("/auth", authRoutes);
 
 export default app;
