9830f1124ed1a835c516e7b503770d5a41a0209d
ryangholland <rghcode@gmail.com>
2025-05-10 13:24:56 -0400

    feat: implement createPost controller using authenticated user

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,7 @@
 import {
   getPublishedPosts,
   getPostByIdService,
+  createPostService,
 } from "../services/postService.js";
 
 export const getAllPosts = async (req, res) => {
@@ -33,7 +34,25 @@ export const getPostById = async (req, res) => {
   }
 };
 
+export const createPost = async (req, res) => {
+  const { title, content, excerpt, published } = req.body;
+
+  try {
+    const newPost = await createPostService({
+      title,
+      content,
+      excerpt,
+      published: !!published,
+      authorId: req.user.userId, // comes from JWT
+    });
+
+    res.status(201).json(newPost);
+  } catch (err) {
+    console.error("Error creating post:", err);
+    res.status(500).json({ message: "Failed to create post" });
+  }
+};
+
 // To Do
-export const createPost = () => {};
 export const updatePost = () => {};
 export const deletePost = () => {};
